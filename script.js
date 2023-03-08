896246d1d9c2f4e02c7789fdd802e4d604fd2ac2
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2023-03-08 22:09:27 +0545

    Front page of the project

---
+++ script.js
@@ -0,0 +1,29 @@
+function lead(n){
+
+    switch(n){
+        case "1":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "2":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "3":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "4":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "5":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "6":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        case "7":
+            location.href = 'heapSort/heapMain.html';
+            break;
+        default:
+            location.href = 'heapSort/heapMain.html';
+            break;
+    }
+}
