11827510ed4e01efa314d6d8263ef19c4570ae1f
ayusao <077bct006.aayusha@pcampus.edu.np>
2023-03-15 18:33:02 +0545

    bubble sort speed and size added

--- bubbleSort/bubble.js
+++ bubbleSort/bubble.js
@@ -1,81 +1,97 @@
-const n=20;
-const array =[];
-var sortingProcess;
-initialize();
-//for generating random values
-function initialize() {
-    // Stop any ongoing sorting process
-    if (sortingProcess) {
-      clearTimeout(sortingProcess);
-    }
-  
-    // Generate a new array of random values
-    for (var i = 0; i < n; i++) {
-      array[i] = Math.random();
-    }
-  
-    showbars();
-  }
-  
+const defaultArraySize = 20;
+const defaultAnimationSpeed = 200;
 
+let arraySize = defaultArraySize;
+let animationSpeed = defaultAnimationSpeed;
+let array = [];
+
+let sortingProcess;
+
+initialize();
+
+// for generating random values
+function initialize() {
+  // Stop any ongoing sorting process
+  if (sortingProcess) {
+    clearTimeout(sortingProcess);
+  }
+
+  // Generate a new array of random values
+  array = Array.from({ length: arraySize }, () => Math.random());
+
+  showbars();
+}
 
 function play() {
-    const copyarray = [...array];
-    const swapping = bubbleSort(copyarray);
-    animate(swapping);
-  }
-  function animate(swaps) {
-    if (swaps.length == 0) {
-      showbars();
-      return;
-    }
-  
-    // Check if a new array has been generated
-    if (swaps.length == 1 && swaps[0][0] == -1) {
-      return;
-    }
-  
-    const [i, j] = swaps.shift();
-    [array[i], array[j]] = [array[j], array[i]];
-    showbars([i, j]);
-  
-    sortingProcess = setTimeout(function () {
-      animate(swaps);
-    }, 200);
-  }
-  
-function bubbleSort(array){
-    const swaps =[];
-        for (var i=0; i<array.length; i++)
-        {
-        for (var j=0; j<= array.length-i; j++)
-        {
-            if (array[j]>array[j+1])
-            {
-                swaps.push([j, j+1]);
-
-                var temp = array[j];
-                array[j] = array[j+1];
-                array[j+1]= temp;
-            }
-        }
-    }
-    return swaps;
+  const copyarray = [...array];
+  const swapping = bubbleSort(copyarray);
+  animate(swapping);
 }
 
-//for displaying the bars
-function showbars(indices){
-    container1.innerHTML = ""; //only one at a time
-    for (var i=0; i<array.length; i++)
-    {
-        const bar = document.createElement("div");
-        bar.style.height = array[i]*100+"%";
-        bar.classList.add("bar");
+function animate(swaps) {
+  if (swaps.length === 0) {
+    showbars();
+    return;
+  }
 
-        if (indices && indices.includes(i)){ //for color to the swapping elements
-            bar.style.backgroundColor = "red";
-        }
-        container1.appendChild(bar); //to add new child to it
-    }
+  // Check if a new array has been generated
+  if (swaps.length === 1 && swaps[0][0] === -1) {
+    return;
+  }
+
+  const [i, j] = swaps.shift();
+  [array[i], array[j]] = [array[j], array[i]];
+  showbars([i, j]);
+
+  sortingProcess = setTimeout(() => {
+    animate(swaps);
+  }, animationSpeed);
 }
 
+function bubbleSort(array) {
+  const swaps = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j <= array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        swaps.push([j, j + 1]);
+
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return swaps;
+}
+
+// for displaying the bars
+function showbars(indices) {
+  const container = document.getElementById("container1");
+  container.innerHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = `${array[i] * 100}%`;
+    bar.classList.add("bar");
+
+    if (indices && indices.includes(i)) {
+      bar.style.backgroundColor = "red";
+    }
+    container.appendChild(bar);
+  }
+}
+
+function onSizeSliderChange(event) {
+  arraySize = Number(event.target.value);
+  initialize();
+}
+
+function onSpeedSliderChange(event) {
+  animationSpeed = Number(event.target.value);
+}
+
+const sizeSlider = document.getElementById("size-slider");
+sizeSlider.addEventListener("change", onSizeSliderChange);
+
+const speedSlider = document.getElementById("speed-slider");
+speedSlider.addEventListener("change", onSpeedSliderChange);
